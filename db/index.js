7983dd1bd456c1e1cc792de3cbe5e9a71a45b31b
Nicole Barranca <nikkibarranca@gmail.com>
2022-05-26 11:45:41 -0400

    add to class and inquirer prompts

--- db/index.js
+++ db/index.js
@@ -14,4 +14,34 @@ class CO {
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id"
       );
   }
+  findRoles() {
+    // do a left join
+    return this.db
+      .promise()
+      .query(
+        "SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department on role.department_id = department.id"
+      );
+  }
+  addDepartment(answers) {
+    return this.db.promise().query("INSERT INTO department SET ?", {
+      name: answers.departmentName,
+    });
+  }
+
+  addNewEmployee(answers) {
+    return this.db.promise().query("INSERT INTO employee SET ?", answers);
+  }
+  addNewRole(answers) {
+    return this.db.promise().query("INSERT INTO role SET ?", answers);
+  }
+  updateEmployeeRole(employeeId, roleId) {
+    return this.connection
+      .promise()
+      .query("UPDATE employee SET role_id = ? WHERE id = ?", [
+        roleId,
+        employeeId,
+      ]);
+  }
 }
+
+module.exports = new CO(db);
--- index.js
+++ index.js
@@ -63,3 +63,182 @@ const init = function () {
       }
     });
 };
+const viewAllDepartments = function () {
+  db.findDepartments()
+    .then(([department]) => {
+      console.table(department);
+    })
+    .then(() => {
+      init();
+    });
+};
+const viewAllEmployees = function () {
+  db.findEmployees()
+    .then(([employee]) => {
+      console.table(employee);
+    })
+    .then(() => {
+      init();
+    });
+};
+const viewAllRoles = function () {
+  db.findRoles()
+    .then(([role]) => {
+      console.table(role);
+    })
+    .then(() => {
+      init();
+    });
+};
+const addNewDepartment = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "departmentName",
+        message: "What is the name of the new department?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      db.addDepartment(answers).then(init());
+    });
+};
+const addNewEmployee = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "What is the first name of the new employee?",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "What is the last name of the new employee?",
+      },
+    ])
+    .then((answers) => {
+      const first = answers.firstName;
+      const last = answers.lastName;
+
+      db.findRoles().then(([role]) => {
+        const roles = role;
+        const listRoles = roles.map(({ title, id }) => ({
+          name: title,
+          value: id,
+        }));
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "employeeRoleId",
+              message: "What is the role of this employee?",
+              choices: listRoles,
+            },
+          ])
+          .then((roles) => {
+            console.log(roles);
+            var roleId = roles.employeeRoleId;
+            let newEmployee = {
+              first_name: first,
+              last_name: last,
+              role_id: roleId,
+            };
+            db.addNewEmployee(newEmployee).then(init());
+          });
+      });
+    });
+};
+
+const updateEmployee = function () {
+  db.findEmployees().then(([employee]) => {
+    const listEmployees = employee.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id,
+    }));
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee?",
+          choices: listEmployees,
+        },
+      ])
+      .then((data) => {
+        var empId = data.employeeId;
+        db.findRoles().then(([role]) => {
+          const roles = role;
+          const listRoles = roles.map(({ title, id }) => ({
+            name: title,
+            value: id,
+          }));
+
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "employeeRoleId",
+                message: "What is the role of this employee?",
+                choices: listRoles,
+              },
+            ])
+            .then((roles) => {
+              console.log(roles);
+              var roleId = roles.employeeRoleId;
+              db.updateEmployeeRole(empId, roleId).then(init());
+            });
+        });
+      });
+  });
+};
+const addNewRole = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of this role?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary for this role?",
+      },
+    ])
+    .then((answers) => {
+      const role = answers.roleName;
+      const money = answers.salary;
+
+      db.findDepartments().then(([department]) => {
+        const listDepartment = department.map(({ id, name }) => ({
+          name: `${name}`,
+          value: id,
+        }));
+
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "deptId",
+              message: "What department is this role in?",
+              choices: listDepartment,
+            },
+          ])
+          .then((departments) => {
+            console.log(departments);
+            var dept = departments.deptId;
+            let newRole = {
+              title: role,
+              salary: money,
+              department_id: dept,
+            };
+            db.addNewRole(newRole).then(init());
+          });
+      });
+    });
+};
+init();
